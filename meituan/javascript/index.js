7b73174e70ea33e6bd54c7cc96c02a070469b464
zshz <m18845128018@163.com>
2017-07-07 15:26:09 +0800

    运用jquerymobile

--- meituan/javascript/index.js
+++ meituan/javascript/index.js
@@ -6,22 +6,52 @@ $(function(){//文档就绪函数
     var $navSpan = $("#nav span");
     //click tap
     $navSpan.on("tap", function(){
-        $(this).addClass("active").siblings().removeClass("active");
-        $guideUl.css({
-            left: - $(this).index() * $guideUl.width() / 2
-        });
+        swipeGuide($(this).index());
     });
     $guideUl.on("swipeleft", function(){
-        $navSpan.eq(1).addClass("active").siblings().removeClass("active");
-        $guideUl.css({
-            left: -$guideUl.width() / 2
-        });
+       swipeGuide(1);
     }).on("swiperight", function(){
-        $navSpan.eq(0).addClass("active").siblings().removeClass("active");
-        $guideUl.css({
-            left: 0
+        swipeGuide(0);
+    });
+    setInterval(function(){
+        if($(window).scrollTop() <= 1){
+            $("#go-top").css('display','none');
+        }else{
+            $("#go-top").css('display','block');
+        }
+    },200);
+
+    $("#go-top").on("tap", function(){
+        $("body").animate({
+            scrollTop : 0
+        }, 500);
+        var top = $(this).css("top");
+        $(this).animate({
+            top: 0
+        }, 500, function(){
+            $(this).css("top", top);
         });
     });
+
+    function swipeGuide(index){
+        $navSpan.eq(index).addClass("active").siblings().removeClass("active");
+        $guideUl.css({
+            left: -index * $guideUl.width() / 2
+        });
+    }
+
+
+
+
+    /*oTop.onclick = function(){
+        var timer = setInterval(function(){
+            var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+            window.scrollTo(0, scrollTop-=50);
+            if(scrollTop <= 0){
+                clearInterval(timer);
+            }
+        }, 100);
+    };*/
 
 
 
--- meituan/javascript/rem.js
+++ meituan/javascript/rem.js
@@ -7,6 +7,9 @@
     function refreshRem() {
         var width = docEl.getBoundingClientRect().width;
         var rem = width / 7.5; // 将屏幕宽度分成7.5份， 1份为1rem
+        if(rem > 50){
+            rem = 50;
+        }
         docEl.style.fontSize = rem + 'px';
     }
 
